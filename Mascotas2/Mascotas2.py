0d4de60eb89ef098120ae14eb9483f6193766dfa
MrPepeGh <chaparromartimezjuancamilo4@gmail.com>
2025-04-28 00:48:21 -0500

    Add files via upload

---
+++ Mascotas2/Mascotas2.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+class Visualizador:
+    def resum(self):
+        clase = self.obtener_clase()
+        return f"|{clase:<6}|{self.nombre:<9}|{self.edad} años |{self.raza:<13}|{self.fecha_ingreso}|"
+class Mascota(Visualizador):
+    def __init__(self, nombre, edad, raza):
+        self.nombre = nombre
+        self.edad = edad
+        self.raza = raza
+        self.fecha_ingreso = datetime.now().astimezone().isoformat()
+    def obtener_clase(self):
+        return self.__class__.__name__
+    '''Ya no necesitamos el metodo "mostrar info" como antes'''
+    '''definimos las clases de las mascotas que se pueden admitir'''
+class Perro(Mascota):
+    pass
+class Gato(Mascota):
+    pass
+class Ave(Mascota):
+    pass
+def ingresar_mascota(numero):
+    while True:
+        clase = input(f"> Mascota {numero}, que clase es (P)Perro, (G)Gato o (A)Ave?\n< ").strip().lower()
+        if clase in ['p', 'g', 'a']:
+            break
+        print("Entrada inválida. Ingresa 'P' para perro, 'G' para gato o 'A' para ave.")
+    '''Hacemos que se identifique qué clase de las 3 es la mascota'''
+    tipo = {"p": "Perro", "g": "Gato", "a": "Ave"}[clase]
+    nombre = input(f"> cual es el nombre del {tipo}?\n< ").strip()
+    edad = int(input(f"> que edad tiene '{nombre}'?\n< "))
+    raza = input(f"> de que raza es '{nombre}'?\n< ").strip()
+    if clase == 'p':
+        return Perro(nombre, edad, raza)
+    elif clase == 'g':
+        return Gato(nombre, edad, raza)
+    else:
+        return Ave(nombre, edad, raza)
+def main():
+    mascotas = []
+    cantidad = int(input("> Cuantas mascotas va a ingresar?\n< "))
+    for i in range(1, cantidad + 1):
+        mascota = ingresar_mascota(i)
+        mascotas.append(mascota)
+    print("\n> Resumen:")
+    print("> |Clase |Nombre   |Edad   |Raza         |Fecha de ingreso          |")
+    for m in mascotas:
+        print("> " + m.resum())
+if __name__ == "__main__":
+    main()
